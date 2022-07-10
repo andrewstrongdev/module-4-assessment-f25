b462c77d792451e154d1a22e4973e3d49e4dd883
Andrew Strong <andrewstrongdev@gmail.com>
2022-07-10 14:00:38 -0600

    finished fortune button

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 const complimentBtn = document.getElementById("complimentButton")
+const fortuneBtn = document.getElementById("fortuneButton")
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
@@ -9,3 +10,14 @@ const getCompliment = () => {
 };
 
 complimentBtn.addEventListener('click', getCompliment)
+
+
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+fortuneBtn.addEventListener('click', getFortune)
--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,14 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
-    }
+    },
+    
+    getFortune: (req, res) => {
+        const fortunes = ["A beautiful, smart, and loving person will be coming into your life.", "A good time to finish up old tasks.", "A hunch is creativity trying to tell you something.", "A person of words and not deeds is like a garden full of weeds.", "A smile is your personal welcome mat.", "A soft voice may be awfully persuasive.", "Advice, when most needed, is least heeded.", "All the effort you are making will ultimately pay off."];
 
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    }
 }
